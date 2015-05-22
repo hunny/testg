5693ab1e01530ce06d63a2eda02320661a0ac366
Hunny.Hu <ghunny57@gmail.com>
2015-05-22 12:44:21 +0800

    Optimizer String Append.

--- src/main/groovy/hh/learng/cloud/tm/GenerateDataMenu.groovy
+++ src/main/groovy/hh/learng/cloud/tm/GenerateDataMenu.groovy
@@ -15,16 +15,18 @@ class GenerateDataMenu {
 			println 'BEGIN';
 			query.eachRow("select * from c_pf_p_012 where sys_zh_cn like '%我的团队信息%'"){ row ->
 				try {
+					def resource_code = "(SELECT resource_code from c_pf_p_012 where sys_zh_cn ='${row.sys_zh_cn}' limit 1)"
 					def nextval = "nextval('c_pf_t_043_pf_043_id_seq'::regclass)"
+					def now = '(SELECT extract(epoch FROM now()))'
 					def sip = "(SELECT pf_005_id FROM c_pf_t_005 WHERE client_code LIKE 'sp%')"
-					println "\tIF NOT EXISTS(SELECT * FROM c_pf_t_043 WHERE resource =(SELECT resource_code from c_pf_p_012 where sys_zh_cn ='${row.sys_zh_cn}' LIMIT 1) AND sys_sip = $sip) THEN"
+					println "\tIF NOT EXISTS(SELECT * FROM c_pf_t_043 WHERE resource = $resource_code AND sys_sip = $sip) THEN"
 					def insert = 'INSERT INTO "public"."c_pf_t_043" ("pf_043_id", "sys_sip", "sys_creator", "sys_crtime", "sys_changer", "sys_chtime", "sys_start", "sys_end", "resource")'
-					println "\t\t$insert VALUES ($nextval, $sip, '10', (SELECT extract(epoch FROM now())), NULL, NULL, NULL, NULL, (SELECT resource_code FROM c_pf_p_012 WHERE sys_zh_cn = '${row.sys_zh_cn}' limit 1));"
+					println "\t\t$insert VALUES ($nextval, $sip, '10', $now, NULL, NULL, NULL, NULL, $resource_code);"
 					println "\tEND IF;"
 					def sipbycode = "(SELECT pf_005_id FROM c_pf_t_005 WHERE client_code in('LS','Schindler','Qoros','Compass','CHIC','Dachser','AtlasCopco','MDLZ','RL'))"
-					println "\tIF NOT EXISTS(SELECT * FROM c_pf_t_043 WHERE resource =(SELECT resource_code from c_pf_p_012 where sys_zh_cn ='${row.sys_zh_cn}' LIMIT 1) AND sys_sip = $sipbycode) THEN"
-					def insert2 = 'INSERT INTO "public"."c_pf_t_043" ("pf_043_id", "sys_sip", "sys_creator", "sys_crtime", "sys_changer", "sys_chtime", "sys_start", "sys_end", "resource")'
-					println "\t\t$insert2 VALUES ($nextval, $sipbycode,(SELECT pf_008_id FROM c_pf_t_008 WHERE user_name in('sp_admin')), (SELECT extract(epoch FROM now())), NULL, NULL, NULL, NULL, (SELECT resource_code FROM c_pf_p_012 WHERE sys_zh_cn = '${row.sys_zh_cn}' limit 1));"
+					println "\tIF NOT EXISTS(SELECT * FROM c_pf_t_043 WHERE resource = $resource_code AND sys_sip = $sipbycode) THEN"
+					def sp_admin = "(SELECT pf_008_id FROM c_pf_t_008 WHERE user_name in('sp_admin'))"
+					println "\t\t$insert VALUES ($nextval, $sipbycode, $sp_admin, $now, NULL, NULL, NULL, NULL, $resource_code);"
 					println "\tEND IF;"
 
 				} catch (Exception e) {
