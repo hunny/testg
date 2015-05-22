45e94105439a2bc9d0f64ad304154130608fe559
Hunny.Hu <ghunny57@gmail.com>
2015-05-22 12:37:20 +0800

    Optimizer

--- src/main/groovy/hh/learng/cloud/tm/GenerateDataMenu.groovy
+++ src/main/groovy/hh/learng/cloud/tm/GenerateDataMenu.groovy
@@ -15,16 +15,6 @@ class GenerateDataMenu {
 			println 'BEGIN';
 			query.eachRow("select * from c_pf_p_012 where sys_zh_cn like '%我的团队信息%'"){ row ->
 				try {
-//					def sip = "(select pf_005_id from c_pf_t_005 where pf_005_id >= 100 and pf_005_id <= 300 order by pf_005_id desc limit 1)";
-//					println "\tIF NOT EXISTS(select * from c_tm_c_000 where tmconobj_type = '${row.tmconobj_type}' and tmconobj_code = '${row.tmconobj_code}') THEN"
-//					println "\t\tINSERT INTO c_tm_c_000 (sys_sip, sys_start, sys_end, tmconobj_type, tmconobj_code) VALUES ($sip, 1388505600, 253402271999, '${row.tmconobj_type}', '${row.tmconobj_code}');";
-//					println "\tEND IF;"
-//					query.eachRow("select * from c_tm_c_001 where tmconobj = ${row.tm_000_id}") {row2->
-//						def tm_000_id = "(select tm_000_id from c_tm_c_000 where tmconobj_type = '${row.tmconobj_type}' and tmconobj_code = '${row.tmconobj_code}' order by tm_000_id asc limit 1)";
-//						println "\tIF NOT EXISTS(select * from c_tm_c_001 where tmconobj = ${tm_000_id} and com_lang = '${row2.com_lang}') THEN"
-//						println "\t\tINSERT INTO c_tm_c_001 (sys_sip, sys_start, sys_end, tmconobj, com_lang, com_langval) VALUES ($sip, 1388505600, 253402271999, $tm_000_id, '${row2.com_lang}', '${row2.com_langval}');"
-//						println "\tEND IF;"
-//					}
 					def nextval = "nextval('c_pf_t_043_pf_043_id_seq'::regclass)"
 					def sip = "(SELECT pf_005_id FROM c_pf_t_005 WHERE client_code LIKE 'sp%')"
 					println "\tIF NOT EXISTS(SELECT * FROM c_pf_t_043 WHERE resource =(SELECT resource_code from c_pf_p_012 where sys_zh_cn ='${row.sys_zh_cn}' LIMIT 1) AND sys_sip = $sip) THEN"
